ac61de0e7c164571b2076083348c0e910737677d
Justin Zeyeum <justinzeyeum@gmail.com>
2024-09-13 16:54:32 -0400

    added bastion and private security groups

--- staging/us-east-2/sec-gp.tf
+++ staging/us-east-2/sec-gp.tf
@@ -1,6 +1,8 @@
+############ Public Security Group #################
 resource "aws_security_group" "Pub_sg" {
   # ... other configuration ...
-  name = "Public-sg"
+  name   = "Public-sg"
+  vpc_id = aws_vpc.staging.id
   ingress {
     from_port        = 80
     to_port          = 80
@@ -29,5 +31,76 @@ resource "aws_security_group" "Pub_sg" {
   }
   tags = {
     Name = "Public-sg"
+  }
+}
+
+############ Bastion Security Group #################
+resource "aws_security_group" "bastion_sg" {
+  # ... other configuration ...
+  name   = "bastion-sg"
+  vpc_id = aws_vpc.staging.id
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ## create before destroy rule ###
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Name = "bastion-sg"
+  }
+}
+
+############ Private Security Group #################
+resource "aws_security_group" "Pri_sg" {
+  # ... other configuration ...
+  name   = "Private-sg"
+  vpc_id = aws_vpc.staging.id
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.Pub_sg.id]
+  }
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.Pub_sg.id]
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ## create before destroy rule ###
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Name = "Private-sg"
   }
 }
