9645fc4021c48d56cb26f16337fb8c5ef3de49bc
Justin Zeyeum <justinzeyeum@gmail.com>
2024-09-26 23:57:00 -0400

    Terraform state lock testing

---
+++ staging/state-lock-test/test1/main.tf
@@ -0,0 +1,9 @@
+## Testing state locking by hardcoding our cidr block ###
+# Create a VPC
+resource "aws_vpc" "staging" {
+  cidr_block = "10.50.0.0/16"
+  tags = {
+    Name        = "staging-vpc"
+    Environment = "staging now"
+  }
+}
