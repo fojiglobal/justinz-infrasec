e2c8efad93befbf0585c968ccecaccb663b1b8c9
Justin Zeyeum <justinzeyeum@gmail.com>
2024-09-27 10:24:43 -0400

    Map of objects to create vpcs and subnets using foreach loops

--- staging/loops/main.tf
+++ staging/loops/main.tf
@@ -5,16 +5,36 @@
 # }
 
 ###  create users using loops ##
-resource "aws_iam_user" "name" {
-  count = length(var.infrasec_users)
-  name  = var.infrasec_users[count.index]
+# resource "aws_iam_user" "name" {
+#   count = length(var.infrasec_users)
+#   name  = var.infrasec_users[count.index]
+# }
+
+# output "user_arn" {
+#   value = aws_iam_user.name[*].arn
+# }
+# ##### Foreach loop for managers
+# resource "aws_iam_user" "managers" {
+#   for_each = toset(var.infrasec_managers)
+#   name     = each.value
+# }
+
+# resource "aws_vpc" "vpcs" {
+#   for_each             = var.vpcs
+#   cidr_block           = each.value["cidr"]
+#   tags                 = each.value["tags"]
+#   enable_dns_hostnames = each.value["enable_dns"]
+# }
+
+resource "aws_vpc" "dr" {
+  cidr_block = "10.110.0.0/16"
 }
 
-output "user_arn" {
-  value = aws_iam_user.name[*].arn
-}
-##### Foreach loop for managers
-resource "aws_iam_user" "managers" {
-  for_each = toset(var.infrasec_managers)
-  name     = each.value
+resource "aws_subnet" "public_subnets" {
+  vpc_id                  = aws_vpc.dr.id
+  for_each                = var.public_subnets
+  cidr_block              = each.value["cidr"]
+  availability_zone       = each.value["azs"]
+  map_public_ip_on_launch = each.value["enable_public_ip"]
+  tags                    = each.value["tags"]
 }
--- staging/loops/variables.tf
+++ staging/loops/variables.tf
@@ -25,3 +25,83 @@ variable "infrasec_managers" {
   type    = list(string)
   default = ["bobama", "jbiden", "kharris"]
 }
+
+#### Map of objects to create vpcs using foreach loops
+variable "vpcs" {
+  type = map(object({
+    cidr       = string
+    tags       = map(string)
+    tenancy    = string
+    enable_dns = bool
+  }))
+  default = {
+    "staging" = {
+      cidr       = "10.50.0.0/16"
+      tenancy    = "default"
+      enable_dns = false
+      tags = {
+        name        = "staging-vpc"
+        Environment = "staging"
+      }
+    }
+    "qa" = {
+      cidr       = "10.60.0.0/16"
+      tenancy    = "default"
+      enable_dns = false
+      tags = {
+        name        = "qa-vpc"
+        Environment = "qa"
+      }
+    }
+    "prod" = {
+      cidr       = "10.70.0.0/16"
+      tenancy    = "default"
+      enable_dns = false
+      tags = {
+        name        = "prod-vpc"
+        Environment = "prod"
+      }
+    }
+  }
+}
+
+
+#### Map of objects to create subnets using foreach loops
+variable "public_subnets" {
+  type = map(object({
+    cidr             = string
+    azs              = string
+    enable_public_ip = bool
+    tags             = map(string)
+  }))
+  default = {
+    "pub-sub1" = {
+      cidr             = "10.110.1.0/24"
+      azs              = "us-east-2a"
+      enable_public_ip = true
+      tags = {
+        name        = "pub-sub1"
+        Environment = "staging"
+      }
+    }
+
+    "pub-sub2" = {
+      cidr             = "10.110.2.0/24"
+      azs              = "us-east-2a"
+      enable_public_ip = true
+      tags = {
+        name        = "pub-sub2"
+        Environment = "staging"
+      }
+    }
+    "pub-sub3" = {
+      cidr             = "10.110.3.0/24"
+      azs              = "us-east-2a"
+      enable_public_ip = true
+      tags = {
+        name        = "pub-sub3"
+        Environment = "staging"
+      }
+    }
+  }
+}
