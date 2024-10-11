de6a0825f758c77233aa6fa9cba63771d174fcc2
Justin Zeyeum <justinzeyeum@gmail.com>
2024-10-11 10:45:37 -0400

    Restrict my bastion ingress to my home ip

--- staging/us-east-2/Old/sec-gp.tf
+++ staging/us-east-2/Old/sec-gp.tf
@@ -43,7 +43,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["192.168.1.132/32"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
