a9d6f065d369e31edcd8cd394e0e4f567039da74
Justin Zeyeum <justinzeyeum@gmail.com>
2024-11-01 00:53:46 -0400

    Fixed vuln code

--- qa/us-east-2/gha-test.tf
+++ qa/us-east-2/gha-test.tf
@@ -9,8 +9,10 @@ resource "aws_vpc" "qa" {
 resource "aws_s3_bucket" "example" {
   bucket = "example"
 }
-resource "aws_s3_bucket" "example" {
-  bucket                  = aws_s3_bucket.example.id
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
