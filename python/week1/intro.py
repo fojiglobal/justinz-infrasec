1ab5a307df47f1de71982473d86780856e19f99a
Justin Zeyeum <justinzeyeum@gmail.com>
2025-02-13 03:42:09 -0500

    Python week1 intro

---
+++ python/week1/intro.py
@@ -0,0 +1,88 @@
+########### Printing Outputs ############
+#print("Hello World")
+
+#print("Hello World"  ": " "Cloud Security 2")
+
+#print("Hello World","Cloud Security 2")
+
+
+########## Variables #################
+
+# name = "Obama"
+# age = 60
+# body_temp = 85.5
+
+#print("My name is ", name, "I am ", age, "years old", "And my body temp is", body_temp, "F")
+
+# print(type(name))
+# print(type(age))
+# print(type(body_temp))
+
+
+
+################# Mathematic Calculations ##################
+
+# numbers_add = 5 + 6
+# numbers_sub = 20 - 5
+# print(numbers_add, numbers_sub)
+
+# year_of_birth = 1990
+# current_year = 2025
+# age = current_year - year_of_birth
+# print(age)
+
+
+######## Input from the user ###############
+# year_of_birth = int(input("please Enter Your Year of Birth: "))
+
+# Current_year = int(input("please Enter The Current Year: "))
+
+
+# age = Current_year - year_of_birth
+# print(age)
+
+
+
+########################## Conditionals ##################
+# year_of_birth = int(input("please Enter Your Year of Birth: "))
+
+# Current_year = int(input("please Enter The Current Year: "))
+
+# age = Current_year - year_of_birth
+
+# if age > 80:
+#     print("You are too old to be a US President")
+# elif age <= 40:
+#     print("You are too Young to be the US President")
+# else:
+#     print("You are Eligible to be a US President")
+
+# print(5+5)
+# print(10-5)
+# print(10%4)
+
+# Write a program that takes a number from a user, 
+# if the number is divisible by 3 print Foo
+# If the number is divisible by 5 print Bar
+# If the number is divisible by both 3 and 5 print Foo Bar
+
+# num = int(input("Please Eneter Any Number: "))
+# if num % 3 == 0 and num % 5 == 0:
+#     print("Foo Bar")
+# elif num % 3 == 0:
+#     print("Foo")
+# elif num % 5 == 0:
+#     print("Bar")
+# else:
+#     print("This", num, "you entered is Neither Divisible by 3 or 5, please try again")
+
+####################### Loops #########################
+
+# num = [1,2,3,4,5,6,7,8,9,10]
+
+# names = {
+#     "first_name" : "Obama",
+#     "Age" : 60
+# }
+# for items in num:
+#     print(items)
